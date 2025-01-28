86cb01d7df8c4909f2ffa96f18c03618731ce246
kaushaki-kumari <kaushaki.kumari@innotechteam.in>
2025-01-28 14:01:51 +0530

    fix conflicts

--- app/utils/userData.tsx
+++ app/utils/userData.tsx
@@ -1,14 +1,14 @@
-  export interface userState {
-    firstName: string;
-    lastName: string;
-    mobileNo: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-    image: string;
-  }
+export interface userState {
+  firstName: string;
+  lastName: string;
+  mobileNo: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  image: string;
+}
 
-  export interface FormData {
-    email: string;
-    password: string;
-  }
+export interface FormData {
+  email: string;
+  password: string;
+}
